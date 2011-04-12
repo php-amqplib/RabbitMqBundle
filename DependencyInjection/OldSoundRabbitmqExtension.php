96db297ad7125e273c20b856ad0007e677209f39
Alvaro Videla <videlalvaro@gmail.com>
2011-04-12 17:22:25 +0200

    fixes Extension for the Symfony PR11

--- DependencyInjection/OldSoundRabbitmqExtension.php
+++ DependencyInjection/OldSoundRabbitmqExtension.php
@@ -2,7 +2,7 @@
 
 namespace OldSound\RabbitMqBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
