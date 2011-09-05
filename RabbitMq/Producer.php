433ad0e3c4a0e06b07978bff55c9edb7f6583146
Alvaro Videla <videlalvaro@gmail.com>
2011-09-05 14:42:03 +0200

    Fixed problem related to message persistency. Thanks to @xosofox for spotting the mistake.

--- RabbitMq/Producer.php
+++ RabbitMq/Producer.php
@@ -9,7 +9,7 @@ class Producer extends BaseAmqp
     public function publish($msgBody, $routingKey = '')
     {
         $this->ch->exchange_declare($this->exchangeOptions['name'], $this->exchangeOptions['type'], false, true, false);
-        $msg = new \AMQPMessage($msgBody, array('content_type' => 'text/plain', 'delivery_mode', 2));
+        $msg = new \AMQPMessage($msgBody, array('content_type' => 'text/plain', 'delivery_mode' => 2));
         $this->ch->basic_publish($msg, $this->exchangeOptions['name'], $routingKey);
     }
 }
