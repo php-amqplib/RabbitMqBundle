bf8aacad9aa0d80661503ee323d96c3b3d7ba048
Alvaro Videla <videlalvaro@gmail.com>
2011-11-25 22:25:30 +0100

    new producer that loads data from STDIN

---
+++ Command/StdInProducerCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OldSound\RabbitMqBundle\Command;
+
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StdInProducerCommand extends BaseRabbitMqCommand
+{
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('rabbitmq:stdin-producer')
+            ->addArgument('name', InputArgument::REQUIRED, 'Producer Name')
+            ->addOption('debug', 'd', InputOption::VALUE_OPTIONAL, 'Enable Debugging', false)
+        ;
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input  An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     *
+     * @return integer 0 if everything went fine, or an error code
+     *
+     * @throws \LogicException When this abstract class is not implemented
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        define('AMQP_DEBUG', (bool) $input->getOption('debug'));
+
+        $producer = $this->getContainer()->get(sprintf('old_sound_rabbit_mq.%s_producer', $input->getArgument('name')));
+
+        $data = '';
+        while (!feof(STDIN)) {
+            $data .= fread(STDIN, 8192);
+        }
+
+        $producer->publish(serialize($data));
+    }
+}
