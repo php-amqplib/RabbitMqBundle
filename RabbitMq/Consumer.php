d7cf1579d50ad382676b3809bc0d4b5057f53691
Panagiotis Kosmidis <panos@hostindex.gr>
2011-12-03 20:36:26 +0200

    Instead of passing only the `$msg->body` to the consumer, pass the whole $msg object.

--- RabbitMq/Consumer.php
+++ RabbitMq/Consumer.php
@@ -28,7 +28,7 @@ class Consumer extends BaseConsumer
         try
         {
             //TODO pass the whole message and document the usage.
-            call_user_func($this->callback, $msg->body);
+            call_user_func($this->callback, $msg);
             $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
             $this->consumed++;
             $this->maybeStopConsumer($msg);
