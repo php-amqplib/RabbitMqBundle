ac049aee6ef52af1f9c99b320e40f643f3bc2263
Matt Drollette <matt@drollette.com>
2012-03-20 14:07:35 -0500

    basic lazy exchange declaring on the producer

--- DependencyInjection/OldSoundRabbitMqExtension.php
+++ DependencyInjection/OldSoundRabbitMqExtension.php
@@ -66,8 +66,6 @@ class OldSoundRabbitMqExtension extends Extension
                 $this->injectLoggedChannel($definition, $key, $producer['connection']);
             }
             $definition->addMethodCall('setExchangeOptions', array($producer['exchange_options']));
-            //TODO add configuration option that allows to not do this all the time.
-            $definition->addMethodCall('exchangeDeclare');
 
             $this->container->setDefinition(sprintf('old_sound_rabbit_mq.%s_producer', $key), $definition);
         }
@@ -177,4 +175,3 @@ class OldSoundRabbitMqExtension extends Extension
         return 'old_sound_rabbit_mq';
     }
 }
-
