e6a91014f4d50f22e806019fa0abe1693214c40d
marc.weistroff <marc.weistroff@sensio.com>
2012-04-24 10:59:22 +0200

    Fixes ConsumerInterface to reflect BCBreak

--- RabbitMq/ConsumerInterface.php
+++ RabbitMq/ConsumerInterface.php
@@ -2,7 +2,10 @@
 
 namespace OldSound\RabbitMqBundle\RabbitMq;
 
+use PhpAmqpLib\Message\AMQPMessage;
+
 interface ConsumerInterface
 {
-    function execute($msg);
+    function execute(AMQPMessage $msg);
 }
+
