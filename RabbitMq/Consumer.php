0a61a6a4c96cd90443e79b871d7681bb6dae05db
Andrey Goryachev <andrey@sixdays.ru>
2012-06-04 15:27:38 +0400

    Removed sleep(1) from bundle code

--- RabbitMq/Consumer.php
+++ RabbitMq/Consumer.php
@@ -24,8 +24,6 @@ class Consumer extends BaseConsumer
         try
         {
             if (false === call_user_func($this->callback, $msg)) {
-                // Pause 1 second. No need load CPU for requeue
-                sleep(1);
                 // Reject and requeue message to RabbitMQ
                 $msg->delivery_info['channel']->basic_reject($msg->delivery_info['delivery_tag'], true);
             }
