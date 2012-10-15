933cfbe6a3f427d08ac95816bc384bddca3a7cbb
Benjamin Dulau <benjamin.dulau@anonymation.com>
2012-10-15 10:11:16 +0200

    Uncommented needed code

--- RabbitMq/BaseConsumer.php
+++ RabbitMq/BaseConsumer.php
@@ -48,35 +48,33 @@ abstract class BaseConsumer extends BaseAmqp
 
     protected function setUpConsumer($consume = true)
     {
-        var_dump($this->routingKey);
+        $this->ch->exchange_declare(
+            $this->exchange->getName(),
+            $this->exchange->getOptions()->get('type'),
+            $this->exchange->getOptions()->get('passive'),
+            $this->exchange->getOptions()->get('durable'),
+            $this->exchange->getOptions()->get('auto_delete'),
+            $this->exchange->getOptions()->get('internal'),
+            $this->exchange->getOptions()->get('nowait'),
+            $this->exchange->getOptions()->get('arguments'),
+            $this->exchange->getOptions()->get('ticket')
+        );
 
-//        $this->ch->exchange_declare(
-//            $this->exchange->getName(),
-//            $this->exchange->getOptions()->get('type'),
-//            $this->exchange->getOptions()->get('passive'),
-//            $this->exchange->getOptions()->get('durable'),
-//            $this->exchange->getOptions()->get('auto_delete'),
-//            $this->exchange->getOptions()->get('internal'),
-//            $this->exchange->getOptions()->get('nowait'),
-//            $this->exchange->getOptions()->get('arguments'),
-//            $this->exchange->getOptions()->get('ticket')
-//        );
-//
-//        list($queueName, ,) = $this->ch->queue_declare(
-//            $this->queue->getName(),
-//            $this->queue->getOptions()->get('passive'),
-//            $this->queue->getOptions()->get('durable'),
-//            $this->queue->getOptions()->get('exclusive'),
-//            $this->queue->getOptions()->get('auto_delete'),
-//            $this->queue->getOptions()->get('nowait'),
-//            $this->queue->getOptions()->get('arguments'),
-//            $this->queue->getOptions()->get('ticket')
-//        );
-//
-//        $this->ch->queue_bind($queueName, $this->exchange->getName(), $this->routingKey);
-//        if ($consume) {
-//            $this->ch->basic_consume($queueName, $this->getConsumerTag(), false, false, false, false, array($this, 'processMessage'));
-//        }
+        list($queueName, ,) = $this->ch->queue_declare(
+            $this->queue->getName(),
+            $this->queue->getOptions()->get('passive'),
+            $this->queue->getOptions()->get('durable'),
+            $this->queue->getOptions()->get('exclusive'),
+            $this->queue->getOptions()->get('auto_delete'),
+            $this->queue->getOptions()->get('nowait'),
+            $this->queue->getOptions()->get('arguments'),
+            $this->queue->getOptions()->get('ticket')
+        );
+
+        $this->ch->queue_bind($queueName, $this->exchange->getName(), $this->routingKey);
+        if ($consume) {
+            $this->ch->basic_consume($queueName, $this->getConsumerTag(), false, false, false, false, array($this, 'processMessage'));
+        }
     }
 
     protected function maybeStopConsumer()
