08d365957452fdde238e27d919e4c91d96d18a9d
Sven Loth <sven.loth@gmail>
2013-04-26 13:38:04 +0200

    consumer can now reject and drop messages

--- RabbitMq/Consumer.php
+++ RabbitMq/Consumer.php
@@ -51,9 +51,15 @@ class Consumer extends BaseConsumer
 
     public function processMessage(AMQPMessage $msg)
     {
-        if (false === call_user_func($this->callback, $msg)) {
+
+        $processFlag = call_user_func($this->callback, $msg);
+
+        if ($processFlag === ConsumerInterface::MSG_REJECT_REQUEUE) {
             // Reject and requeue message to RabbitMQ
             $msg->delivery_info['channel']->basic_reject($msg->delivery_info['delivery_tag'], true);
+        } else if ($processFlag === ConsumerInterface::MSG_REJECT) {
+            // Reject and drop
+            $msg->delivery_info['channel']->basic_reject($msg->delivery_info['delivery_tag'], false);
         } else {
             // Remove message from queue only if callback return not false
             $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
