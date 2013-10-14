b8e21bdedbc7a51a6d47739343698cba4db16cce
Daan van den Berg <info@doppy.nl>
2013-10-14 15:44:22 +0200

    adds debug flag to setup-fabric command.

--- Command/SetupFabricCommand.php
+++ Command/SetupFabricCommand.php
@@ -12,11 +12,17 @@ class SetupFabricCommand extends BaseRabbitMqCommand
         $this
             ->setName('rabbitmq:setup-fabric')
             ->setDescription('Sets up the Rabbit MQ fabric')
+            ->addOption('debug', 'd', InputOption::VALUE_NONE, 'Enable Debugging')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if (defined('AMQP_DEBUG') === false) {
+            define('AMQP_DEBUG', (bool) $input->getOption('debug'));
+        }
+
+
         $output->writeln('Setting up the Rabbit MQ fabric');
 
         $partsHolder = $this->getContainer()->get('old_sound_rabbit_mq.parts_holder');
