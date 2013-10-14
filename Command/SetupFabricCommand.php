273afbb08ed5f4ebb32418e532c6b7eca0f9fe11
Daan van den Berg <info@doppy.nl>
2013-10-14 15:51:06 +0200

    adds debug flag to setup-fabric command.

--- Command/SetupFabricCommand.php
+++ Command/SetupFabricCommand.php
@@ -3,6 +3,7 @@
 namespace OldSound\RabbitMqBundle\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SetupFabricCommand extends BaseRabbitMqCommand
