20d6a6ca889d99516571df2765a77981cc514a56
Charles-Eric Gorron <charles-eric.gorron@francetv.fr>
2015-01-28 18:36:25 +0100

    Review Symfony components dependencies: HttpKernel and FrameworkBundle are not necessary for a simple console application.

--- Command/BaseRabbitMqCommand.php
+++ Command/BaseRabbitMqCommand.php
@@ -2,8 +2,30 @@
 
 namespace OldSound\RabbitMqBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand as Command;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
 
-abstract class BaseRabbitMqCommand extends Command
+abstract class BaseRabbitMqCommand extends Command implements ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
 }
