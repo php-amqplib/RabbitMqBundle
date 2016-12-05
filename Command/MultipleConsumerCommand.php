0d35a82506f03499d03a186a3f40bc49216f56b6
Tibor Barna <tibor.barna@wiredminds.de>
2016-12-05 16:56:57 +0100

    Added optional context argument to multiple consumer command
    Added unit tests for multiple consumer

--- Command/MultipleConsumerCommand.php
+++ Command/MultipleConsumerCommand.php
@@ -2,18 +2,28 @@
 
 namespace OldSound\RabbitMqBundle\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
+
 class MultipleConsumerCommand extends BaseConsumerCommand
 {
     protected function configure()
     {
         parent::configure();
 
-        $this->setDescription('Executes a consumer that uses multiple queues');
-        $this->setName('rabbitmq:multiple-consumer');
+        $this->setDescription('Executes a consumer that uses multiple queues')
+                ->setName('rabbitmq:multiple-consumer')
+                ->addArgument('context', InputArgument::OPTIONAL, 'Context the consumer runs in')
+        ;
     }
 
     protected function getConsumerService()
     {
         return 'old_sound_rabbit_mq.%s_multiple';
+    }
+
+    protected function initConsumer($input)
+    {
+        parent::initConsumer($input);
+        $this->consumer->setContext($input->getArgument('context'));
     }
 }
