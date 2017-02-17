b2fa8bdff1f6b3941ba7faf0c464540ef746c7ba
AgentSIB <agentsib@gmail.com>
2017-02-17 22:41:48 +0300

    Fix ram checker

--- RabbitMq/Consumer.php
+++ RabbitMq/Consumer.php
@@ -175,6 +175,6 @@ class Consumer extends BaseConsumer
     {
         $memoryManager = new MemoryConsumptionChecker(new NativeMemoryUsageProvider());
 
-        return $memoryManager->isRamAlmostOverloaded($this->getMemoryLimit(), '5M');
+        return $memoryManager->isRamAlmostOverloaded('5M', $this->getMemoryLimit().'M');
     }
 }
