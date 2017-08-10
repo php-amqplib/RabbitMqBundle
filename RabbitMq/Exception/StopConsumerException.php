7ca1c42c278cc75e67f5777fdfa1e1fc1fac2b25
Vinícius de Sá Souza <viniciusss@me.com>
2017-08-10 17:21:09 -0300

    Fix typo
    RestartConsumerException  -> StopConsumerException

--- RabbitMq/Exception/StopConsumerException.php
+++ RabbitMq/Exception/StopConsumerException.php
@@ -7,7 +7,7 @@ use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
  * If this exception is thrown in consumer service the message
  * will not be ack and consumer will stop
  * if using demonized, ex: supervisor, the consumer will actually restart
- * Class RestartConsumerException
+ * Class StopConsumerException
  * @package OldSound\RabbitMqBundle\RabbitMq\Exception
  */
 class StopConsumerException extends \RuntimeException
