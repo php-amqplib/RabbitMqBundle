95f23bb5492d29cea304d5525ac00ea04239a991
ilyes kooli <kooli.ilyes@gmail.com>
2018-05-02 16:05:49 +0300

    Remove duplicate $definition->setPublic(true);

--- DependencyInjection/OldSoundRabbitMqExtension.php
+++ DependencyInjection/OldSoundRabbitMqExtension.php
@@ -387,7 +387,6 @@ class OldSoundRabbitMqExtension extends Extension
     {
         foreach ($this->config['batch_consumers'] as $key => $consumer) {
             $definition = new Definition('%old_sound_rabbit_mq.batch_consumer.class%');
-            $definition->setPublic(true);
 
             if (!isset($consumer['exchange_options'])) {
                 $consumer['exchange_options'] = $this->getDefaultExchangeOptions();
@@ -441,8 +440,7 @@ class OldSoundRabbitMqExtension extends Extension
     protected function loadAnonConsumers()
     {
         foreach ($this->config['anon_consumers'] as $key => $anon) {
-            $definition = new Definition('%old_sound_rabbit_mq.anon_consumer.class%');
-            $definition->setPublic(true);
+            $definition = new Definition('%old_sound_rabbit_mq.anon_consumer.class%');     
             $definition
                 ->setPublic(true)
                 ->addTag('old_sound_rabbit_mq.base_amqp')
