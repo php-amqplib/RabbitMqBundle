php-amqplib/RabbitMqBundle

commit a550c401dcb9635f1303f6aca751c3ec61794c72

Fix Deprecations SF4.1 (#571)
Fix deprecations

DependencyInjection/Configuration.php
@@ -30,9 +30,8 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $tree = new TreeBuilder();
-
-        $rootNode = $tree->root($this->name);
+        $tree = new TreeBuilder($this->name);
+        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode') ? $tree->getRootNode() : $tree->root($this->name);
 
         $rootNode
             ->children()

commit b8099adef0bc26096b22ad9eab188f8e6bd5b5aa

make code compatible with php 5.3
the builds are failing on php5.3 due the fact that php 5.3 doesn't support ::class constant.
The issue was introduced by: https://github.com/php-amqplib/RabbitMqBundle/pull/571/files

DependencyInjection/Configuration.php
@@ -31,7 +31,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $tree = new TreeBuilder($this->name);
-        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode') ? $tree->getRootNode() : $tree->root($this->name);
+        $rootNode = \method_exists('Symfony\Component\Config\Definition\Builder\TreeBuilder', 'getRootNode') ? $tree->getRootNode() : $tree->root($this->name);
 
         $rootNode
             ->children()