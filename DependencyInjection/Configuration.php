php-amqplib/RabbitMqBundle

commit a550c401dcb9635f1303f6aca751c3ec61794c72

Fix Deprecations SF4.1 (#571)
Fix deprecations

DependencyInjection/Configuration.php
@@ -30,9 +30,8 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $tree = new TreeBuilder();
-
-        $rootNode = $tree->root($this->name);
+        $tree = new TreeBuilder($this->name);
+        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode') ? $tree->getRootNode() : $tree->root($this->name);
 
         $rootNode
             ->children()

commit 5d46fd892e5f6ac0540195846fbcf32c8086bf74

add php 7.3 to build
* add php7.3 in travis checking

* make code compatible with php 5.3

the builds are failing on php5.3 due the fact that php 5.3 doesn't support ::class constant.
The issue was introduced by: https://github.com/php-amqplib/RabbitMqBundle/pull/571/files

.travis.yml
@@ -3,8 +3,6 @@ language: php
 sudo: false
 
 php:
-  - 7.2
-  - 7.1
   - 7.0
   - 5.6
   - 5.6
@@ -34,9 +32,11 @@ matrix:
     - php: 7.0
       env: SYMFONY_VERSION=3.0.*
     - php: 7.1
-      env: DEPENDENCIES=beta
+      env: SYMFONY_VERSION=3.0.*
     - php: 7.2
-      env: DEPENDENCIES=beta
+      env: SYMFONY_VERSION=3.0.*
+    - php: 7.3
+      env: SYMFONY_VERSION=3.0.*
 
   allow_failures:
     - php: nightly
@@ -52,7 +52,7 @@ before_install:
   - if [ "$COMPOSER_FLAGS" != "" ]; then composer update --prefer-dist --no-interaction --no-scripts $COMPOSER_FLAGS; fi;
 
 install:
-  - composer install --prefer-dist
-
+  - COMPOSER_MEMORY_LIMIT=-1 travis_retry composer install --prefer-dist --no-interaction
+  
 script:
   - vendor/bin/phpunit

DependencyInjection/Configuration.php
@@ -31,7 +31,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $tree = new TreeBuilder($this->name);
-        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode') ? $tree->getRootNode() : $tree->root($this->name);
+        $rootNode = \method_exists('Symfony\Component\Config\Definition\Builder\TreeBuilder', 'getRootNode') ? $tree->getRootNode() : $tree->root($this->name);
 
         $rootNode
             ->children()