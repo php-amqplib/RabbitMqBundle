95caa3f4f1355d9aed1991ea40849fdbbad2e5de
mihaileu <mihai.leu@hotmail.com>
2021-03-16 21:29:31 +0200

    Revert "Merge pull request #424 from mbiagetti/master"
    This reverts commit 187d4486bb9cfd34d8b8491dcc2d2804ca9dea95, reversing
    changes made to aaefc33189566bd28d54b2f868d30c7f76a9840e.

--- RabbitMq/Fallback.php
+++ RabbitMq/Fallback.php
@@ -4,15 +4,7 @@ namespace OldSound\RabbitMqBundle\RabbitMq;
 
 class Fallback implements ProducerInterface
 {
-    /**
-     * Publish a message
-     *
-     * @param string $msgBody
-     * @param string $routingKey
-     * @param array $additionalProperties
-     * @param array $headers
-     */
-    public function publish($msgBody, $routingKey = '', $additionalProperties = array(), array $headers = null)
+    public function publish($msgBody, $routingKey = '', $additionalProperties = array())
     {
         return false;
     }
--- RabbitMq/ProducerInterface.php
+++ RabbitMq/ProducerInterface.php
@@ -10,7 +10,6 @@ interface ProducerInterface
      * @param string $msgBody
      * @param string $routingKey
      * @param array $additionalProperties
-     * @param array $headers
      */
-    public function publish($msgBody, $routingKey = '', $additionalProperties = array(), array $headers = null);
+    public function publish($msgBody, $routingKey = '', $additionalProperties = array());
 }
