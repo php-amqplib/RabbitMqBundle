7a8d7c91f1642c684f4476e6d82b44eab607b63a
Eloar <eloar@itchaos.pl>
2021-12-17 08:34:14 +0100

    Symfony 5.4 deprecations adressed

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -29,7 +29,7 @@ class Configuration implements ConfigurationInterface
         $this->name = $name;
     }
 
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $tree = new TreeBuilder($this->name);
         /** @var ArrayNodeDefinition $rootNode */
