a86783b2cf8653872eb895d12dda29f9c3f2f139
mihaileu <mihaileu@users.noreply.github.com>
2022-02-03 11:34:47 +0000

    Apply php-cs-fixer changes

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,14 +13,14 @@ declare(strict_types=1);
  */
 
 $header = <<<'EOF'
-This file is part of PHP CS Fixer.
+    This file is part of PHP CS Fixer.
 
-(c) Fabien Potencier <fabien@symfony.com>
-    Dariusz Rumiński <dariusz.ruminski@gmail.com>
+    (c) Fabien Potencier <fabien@symfony.com>
+        Dariusz Rumiński <dariusz.ruminski@gmail.com>
 
-This source file is subject to the MIT license that is bundled
-with this source code in the file LICENSE.
-EOF;
+    This source file is subject to the MIT license that is bundled
+    with this source code in the file LICENSE.
+    EOF;
 
 $finder = PhpCsFixer\Finder::create()
     ->ignoreDotFiles(false)
